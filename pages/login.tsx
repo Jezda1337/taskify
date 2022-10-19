8a251fbfc2e22e76134af5acf12434b7c01eb099
Radoje <radoje.jezdic@single.earth>
2022-10-19 13:37:13 +0200

    adding new reusable form for both login and register pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@mui/material";
-import MyFormControl from "components/shared/MyFormControl";
+import AuthForm from "@/components/shared/AuthForm";
 import useForm from "hooks/useForm";
 import { FormControls } from "types/auth/form-controls.type";
 import { Validators } from "utils/Validators";
@@ -45,34 +44,14 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen p-4 max-w-md m-auto ">
-        <h3 className="text-2xl mb-8">Login</h3>
-        <form className="w-full flex flex-col gap-4 justify-center ">
-          {Object.entries(inputs).map(([key, formCtrl]) => (
-            <MyFormControl
-              type={formCtrl.type}
-              key={key}
-              elementType={formCtrl.elementType}
-              required={formCtrl.required}
-              value={formCtrl.value}
-              label={formCtrl.label}
-              error={!formCtrl.valid && formCtrl.touched && formCtrl.blurred}
-              inputHandler={e => setInputs(e.target.value, key)}
-              blurHandler={e => {setInputs(e.target.value, key, true)}}
-              checkboxHandler={e => setInputs(!!e.target.checked, key)}
-            />
-          ))}
-          <Button
-            type="submit"
-            fullWidth
-            variant="outlined"
-            disabled={!formIsValid}
-            className="mt-4"
-          >
-            Login
-          </Button>
-        </form>
-      </div>
+      <AuthForm
+        title="Login"
+        btnText="Login"
+        inputs={inputs}
+        blur={true}
+        setInputs={setInputs}
+        formIsValid={formIsValid}
+      />
     </>
   );
 };
--- pages/register.tsx
+++ pages/register.tsx
@@ -1,5 +1,4 @@
-import MyFormControl from "@/components/shared/MyFormControl";
-import { Button } from "@mui/material";
+import AuthForm from "@/components/shared/AuthForm";
 import useForm from "hooks/useForm";
 import { FormControls } from "types/auth/form-controls.type";
 import { Validators } from "utils/Validators";
@@ -63,35 +62,14 @@ const Register = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen p-4 max-w-md m-auto ">
-        <h3 className="text-2xl mb-8">Register</h3>
-        <form className="w-full flex flex-col gap-4 justify-center ">
-          {Object.entries(inputs).map(([key, formCtrl]) => (
-            <MyFormControl
-              type={formCtrl.type}
-              key={key}
-              elementType={formCtrl.elementType}
-              required={formCtrl.required}
-              value={formCtrl.value}
-              label={formCtrl.label}
-              error={!formCtrl.valid && formCtrl.touched && formCtrl.blurred}
-              inputHandler={e => setInputs(e.target.value, key)}
-              blurHandler={e => {setInputs(e.target.value, key, true)}}
-              checkboxHandler={e => setInputs(!!e.target.checked, key)}
-            />
-          ))}
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="outlined"
-            disabled={!formIsValid}
-            className="mt-4"
-          >
-            Register
-          </Button>
-        </form>
-      </div>
+      <AuthForm
+        inputs={inputs}
+        setInputs={setInputs}
+        formIsValid={formIsValid}
+        title="Register"
+        btnText="Register"
+        blur={true}
+      />
     </>
   );
 };
