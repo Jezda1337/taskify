ef5db19c10e9f125c3a12e75f987d97be957e741
Radoje <radoje.jezdic@single.earth>
2022-10-21 15:56:05 +0200

    return form tag to authForm component

--- components/pages/auth/AuthLayout.tsx
+++ components/pages/auth/AuthLayout.tsx
@@ -2,9 +2,7 @@ const AuthLayout = ({ children }) => {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen p-4 max-w-md m-auto ">
-        <form className="w-full flex flex-col gap-4 justify-center ">
-          {children}
-        </form>
+        {children}
       </div>
     </>
   );
